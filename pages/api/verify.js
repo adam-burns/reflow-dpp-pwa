5883758f827296594b067c04ff2bb23e52e43b03
vortex <adamb@free2air.net>
2025-09-08 17:54:32 +0200

    updates server details to lauds

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -5,16 +5,16 @@ import {
 const verifySignature = async (data) => {
   const contract = `Scenario 'ecdh': verifies the signature
 
-Given I have a 'public key' from 'https://reflow-demo.dyne.org/api/json/trace'
+Given I have a 'public key' from 'https://interfacer-staging.dyne.im/api/json/trace'
 Given I have a 'string dictionary' named 'DPP'
 Given I have a 'signature' named 'DPP.signature'
 
-When I verify the 'DPP' has a signature in 'DPP.signature' by 'https://reflow-demo.dyne.org/api/json/trace'
+When I verify the 'DPP' has a signature in 'DPP.signature' by 'https://interfacer-staging.dyne.im/api/json/trace'
 
 Then print the string 'The Reflow DPP signature was verified'
 Then print the 'DPP'
 Then print the 'DPP.signature'`
-  const keys = '{"https://reflow-demo.dyne.org/api/json/trace":{"public_key":"BBCQg21VcjsmfTmNsg+I+8m1Cm0neaYONTqRnXUjsJLPa8075IYH+a9w2wRO7rFM1cKmv19Igd7ntDZcUvLq3xI="}}';
+  const keys = '{"https://interfacer-staging.dyne.im/api/json/trace":{"public_key":"BBCQg21VcjsmfTmNsg+I+8m1Cm0neaYONTqRnXUjsJLPa8075IYH+a9w2wRO7rFM1cKmv19Igd7ntDZcUvLq3xI="}}';
   return await zencode_exec(contract, {
     keys,
     data: JSON.stringify(data)
