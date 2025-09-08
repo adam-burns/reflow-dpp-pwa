f80ad99870e41347860125f7b0b8c149b7ed7ddf
vortex <adamb@free2air.net>
2025-09-08 17:58:12 +0200

    adds cosmetic pwa page changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,45 +6,35 @@ export default function Home() {
   return (
     <Page>
       <Navbar
-        title="Reflow DPP APP"
+        title="LAUDS DPP APP"
       />
       <Head>
-        <title>Reflow DPP App</title>
+        <title>LAUDS DPP App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <h1 className="px-2 my-3 text-2xl font-bold text-center">
-        🤗 to the <a href="https://reflowos.dyne.org" target="_blank" rel="noopener noreferrer" className="text-green-400">Reflow</a> DPP App
+        🤗 to the <a href="https://reflowos.dyne.org" target="_blank" rel="noopener noreferrer" className="text-green-400">LAUDS</a> DPP App
       </h1>
-
       <p className="p-4 m-3 text-sm">
-        This app is ment to verify and generate Digital product passports part of the <a href="https://reflowproject.eu" target="_blank" rel="noopener noreferrer"> Reflow project </a>
+        This app is used to generate and verify Digital Product Passports as part of the <a href="https://lauds.eu" target="_blank" rel="noopener noreferrer"> LAUDS project </a>
       </p>
-
       <div className="flex flex-col ">
         <div className="max-w-md p-8 m-4 bg-white border rounded-lg shadow hover:text-green-400 hover:border-green-400 hover:shadow-xl">
           <Link href="/generate">
-            <a>
-              <h2 className="text-3xl font-semibold">Generate &rarr;</h2>
-              <p className="mt-2 text-xl">Generate a Digital Product Passport and a QR Code</p>
-            </a>
+            <p className="mt-2 text-xl">Generate a Digital Product Passport and a QR Code</p>
           </Link>
         </div>
 
         <div className="max-w-md p-8 m-4 bg-white border rounded-lg shadow hover:text-green-400 hover:border-green-400 hover:shadow-xl">
           <Link href="/verify">
-            <a>
-              <h2 className="text-3xl font-semibold">Verify &rarr;</h2>
-              <p className="mt-2 text-xl">Verify the information by scanning the QR Code</p>
-            </a>
+            <p className="mt-2 text-xl">Verify the information by scanning the QR Code</p>
           </Link>
         </div>
       </div>
-
       <footer className="p-4 text-center">
         <span>Powered by <a target="_blank" rel="noopener noreferrer" href="https://dyne.org" className="font-medium text-green-300 hover:underline font-dyne">Dyne.org</a>.
         </span>
       </footer>
     </Page>
-  )
+  );
 }
